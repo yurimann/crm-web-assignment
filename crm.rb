844a4d0940dc1904c5378fff0b0c6853be0f058e
Yuri <yuri.manahan@gmail.com>
2017-01-24 13:52:16 -0500

    Contacts class linked to CRM

--- crm.rb
+++ crm.rb
@@ -2,8 +2,17 @@
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
 require 'sinatra'
+require_relative 'contact'
+
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
   @crm_app_name = "Yuri's CRM"
   erb :index
+end
+
+get '/contacts' do
+ erb :contacts
 end
