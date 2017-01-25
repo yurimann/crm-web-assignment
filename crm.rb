328f7d5a7f3bf80dc19f3041f3d06647cb6b7f88
Yuri <yuri.manahan@gmail.com>
2017-01-25 13:59:58 -0500

    Edit function completed

--- crm.rb
+++ crm.rb
@@ -39,7 +39,34 @@ post '/contacts/delete' do
   redirect to('/contacts')
 end
 
-get '/contacts/1900' do
-  @contact = Contact.find(1900)
-  erb :show_contact
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
 end
