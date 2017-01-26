d9c51b76a7afd787937e90d5dabedca9b125b99c
Yuri <yuri.manahan@gmail.com>
2017-01-26 12:51:40 -0500

    Linked to SQLite3

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem 'activerecord', '= 4.2.7'
+gem 'mini_record'
--- crm.rb
+++ crm.rb
@@ -5,10 +5,10 @@ require 'sinatra'
 require_relative 'contact'
 use Rack::MethodOverride
 
-Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
-Contact.create('Betty', 'Maker', 'betty@bitmakerlabs.com', 'Developer')
+# Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+# Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+# Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+# Contact.create('Betty', 'Maker', 'betty@bitmakerlabs.com', 'Developer')
 
 get '/' do
   @crm_app_name = "Yuri's CRM"
@@ -78,5 +78,9 @@ delete '/contacts/:id' do
     redirect to('/contacts')
   else
     raise Sinatra::NotFound
+  end
+
+  after do
+    ActiveRecord::Base.connection.close
   end
 end
